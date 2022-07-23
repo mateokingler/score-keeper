5e6f211231d4d18363ca25c8ed1bc48a0c5866f2
erniegomila <109680926+erniegomila@users.noreply.github.com>
2022-07-22 23:17:21 -0400

    Constraints on Color Well

--- ScoreKeeper/TeamEditViewController.swift
+++ ScoreKeeper/TeamEditViewController.swift
@@ -22,42 +22,35 @@ class TeamEditViewController: UIViewController, UIColorPickerViewControllerDeleg
     @IBOutlet weak var team2Input: UITextField!
     @IBOutlet weak var team2ColorLabel: UILabel!
     
-    @IBOutlet weak var teamColorButton: UIButton!
-    @IBOutlet weak var team2ColorImageView: UIImageView!
     
-    @IBOutlet weak var team1ColorImageView: UIImageView!
-    var button1Pressed: Bool = false
-    var button2Pressed: Bool = false
+    @IBOutlet weak var teamColorButton: UIButton!
+    
+    @IBOutlet weak var team1ColorWell: UIColorWell!
+    @IBOutlet weak var team2ColorWell: UIColorWell!
 
     var sportSelected : String = ""
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "\(sportSelected) Team Customization"
         team1Label.text = "Team 1"
         team2Label.text = "Team 2"
-        
         team1NameLabel.text = "Team Name"
         team2NameLabel.text = "Team Name"
-        
         team1ColorLabel.text = "Team Color"
         team2ColorLabel.text = "Team Color"
+
         
-        //teamColorButton.backgroundColor = UIColor.red
-        //team2ColorButton.backgroundColor = UIColor.blue
+        team1ColorWell.backgroundColor = .systemRed
+        team2ColorWell.backgroundColor = .systemBlue
         
-        button1Pressed = false
-        button2Pressed = false
-        
-        team1ColorImageView.backgroundColor = .systemRed
-        team2ColorImageView.backgroundColor = .systemBlue
-        
-        team1ColorImageView.layer.cornerRadius = 8.0
-        team2ColorImageView.layer.cornerRadius = 8.0
-        team1ColorImageView.layer.borderWidth = 2
-        team2ColorImageView.layer.borderWidth = 2
-        
+        team1ColorWell.layer.cornerRadius = 8.0
+        team2ColorWell.layer.cornerRadius = 8.0
+        team1ColorWell.layer.borderWidth = 2
+        team2ColorWell.layer.borderWidth = 2
+       
+        team1ColorWell.title = "Select Color"
         
         team1Label.sizeToFit()
         team2Label.sizeToFit()
@@ -65,11 +58,9 @@ class TeamEditViewController: UIViewController, UIColorPickerViewControllerDeleg
         team2NameLabel.sizeToFit()
         team1ColorLabel.sizeToFit()
         team2ColorLabel.sizeToFit()
-        colorWell1.backgroundColor = .systemRed
-        view.addSubview(colorWell1)
-        view.addSubview(colorWell2)
-        colorWell1.addTarget(self, action: #selector(colorChanged), for: .valueChanged)
-        colorWell2.addTarget(self, action: #selector(colorChanged), for: .valueChanged)
+       
+        team1ColorWell.addTarget(self, action: #selector(colorWellChanged(_:)), for: .valueChanged)
+        team2ColorWell.addTarget(self, action: #selector(colorWellChanged(_:)), for: .valueChanged)
         
         // Do any additional setup after loading the view.
         //Looks for single or multiple taps.
@@ -83,6 +74,8 @@ class TeamEditViewController: UIViewController, UIColorPickerViewControllerDeleg
         
     }
     
+  
+
     //LOCK PORTRAIT MODE -> Method in AppDelegate
     override func viewWillAppear(_ animated: Bool) {
         AppDelegate.AppUtility.lockOrientation(UIInterfaceOrientationMask.portrait, andRotateTo: UIInterfaceOrientation.portrait)
@@ -94,35 +87,10 @@ class TeamEditViewController: UIViewController, UIColorPickerViewControllerDeleg
         view.endEditing(true)
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        colorWell1.translatesAutoresizingMaskIntoConstraints = true
-        colorWell2.translatesAutoresizingMaskIntoConstraints = true
-       
-        colorWell1.frame = CGRect(x: 38, y: 460, width: 0, height: 0)
-        colorWell2.frame = CGRect(x: 260, y: 460, width: 0, height: 0)
-    }
-   
-    let colorWell1: UIColorWell = {
-        let colorWell1 = UIColorWell()
-        colorWell1.backgroundColor = .systemRed
-        colorWell1.title = "Team 1 Color"
-
-        return colorWell1
-    }()
-    
-    let colorWell2: UIColorWell = {
-       let colorWell2 = UIColorWell()
-        colorWell2.backgroundColor = .systemBlue
-        colorWell2.title = "Team 2 Color"
-        return colorWell2
-    }()
-    
-    @objc private func colorChanged(){
-        team1ColorImageView.backgroundColor = colorWell1.selectedColor
-        colorWell1.backgroundColor = colorWell1.selectedColor
-        team2ColorImageView.backgroundColor = colorWell2.selectedColor
-        colorWell2.backgroundColor = colorWell2.selectedColor
+    @objc func colorWellChanged(_ sender: Any) {
+        team1ColorWell.backgroundColor = team1ColorWell.selectedColor
+        team2ColorWell.backgroundColor = team2ColorWell.selectedColor
+        
     }
 
    
@@ -137,15 +105,15 @@ class TeamEditViewController: UIViewController, UIColorPickerViewControllerDeleg
             let sportVC = storyboard?.instantiateViewController(withIdentifier: "BasketballViewController") as! BasketballViewController
             sportVC.team1Name = team1Input.text ?? "Team 1"
             sportVC.team2Name = team2Input.text ?? "Team 2"
-            sportVC.team1Color = colorWell1.selectedColor ?? .systemRed
-            sportVC.team2Color = colorWell2.selectedColor ?? .systemBlue
+            //sportVC.team1Color = colorWell1.selectedColor ?? .systemRed
+           // sportVC.team2Color = colorWell2.selectedColor ?? .systemBlue
             navigationController?.pushViewController(sportVC, animated: true)
         }else if(sportSelected == "Hockey"){
             let sportVC = storyboard?.instantiateViewController(withIdentifier: "HockeyViewController") as! HockeyViewController
             sportVC.team1Name = team1Input.text ?? "Team 1"
             sportVC.team2Name = team2Input.text ?? "Team 2"
-            sportVC.team1Color = colorWell1.selectedColor ?? .systemRed
-            sportVC.team2Color = colorWell2.selectedColor ?? .systemBlue
+            //sportVC.team1Color = colorWell1.selectedColor ?? .systemRed
+            //sportVC.team2Color = colorWell2.selectedColor ?? .systemBlue
             navigationController?.pushViewController(sportVC, animated: true)
         }else if(sportSelected == "Soccer"){
             let sportVC = storyboard?.instantiateViewController(withIdentifier: "SoccerViewController") as! SoccerViewController
@@ -163,13 +131,13 @@ class TeamEditViewController: UIViewController, UIColorPickerViewControllerDeleg
         
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        //let nextVC = storyboard?.instantiateViewController(withIdentifier: "\(sportSelected)ViewController") as! BaseballViewController
-        if(segue.identifier == "editToBasketball"){
-            let nextVC = storyboard?.instantiateViewController(withIdentifier: "BasketballViewController") as! BasketballViewController
-            nextVC.team1Name = "test"
-        }
-    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        //let nextVC = storyboard?.instantiateViewController(withIdentifier: "\(sportSelected)ViewController") as! BaseballViewController
+//        if(segue.identifier == "editToBasketball"){
+//            let nextVC = storyboard?.instantiateViewController(withIdentifier: "BasketballViewController") as! BasketballViewController
+//            nextVC.team1Name = "test"
+//        }
+//    }
     /*
     // MARK: - Navigation
 
